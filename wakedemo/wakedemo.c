839a70f7e983823b59062978adae8058ee7d210a
WokyDoky <daniel.benavent3@gmail.com>
2024-12-01 12:18:40 -0700

    Test new padel

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -114,7 +114,7 @@ void screen_update_second_ball(){
 
 void draw_paddle(int x, int y, unsigned short color) {
   fillRectangle(x, y, PADDLE_WIDTH, PADDLE_HEIGHT, color);
-  fillRectangle(x, y, PADDLE_WIDTH, PADDLE_HEIGHT + SCREEN_HEIGHT - 20, color);
+  fillRectangle(x, y + SCREEN_HEIGHT - 20, PADDLE_WIDTH, PADDLE_HEIGHT, color);
 }
 
 // Function to update paddle position
